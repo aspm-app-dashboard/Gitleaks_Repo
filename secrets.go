7a2b5b7798c7e6ecab1af62119224a0967f1580d
cbp-qa-automation <cbp-qa-automation@beescloud.com>
2025-12-03 12:49:51 +0530

    Add secrets.go for Gitleaks testing
    Added intentional secrets for Gitleaks testing.

---
+++ secrets.go
@@ -0,0 +1,6 @@
+package secrets
+
+// Intentional secrets for Gitleaks testing
+var APIKey = "AIzaSyA-FAKE-KEY"
+var AWSAccessKey = "AKIAFAKE1234567890"
+var GitHubToken = "ghp_FAKEGITHUBTOKEN123456789"
